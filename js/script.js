9dc5af3f8f17594420058d60e432bc3307d146a6
Michelle- <michukimichelle@gmail.com>
2020-02-24 17:31:33 +0300

    added submission form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+$("#submit").click(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var mail = $("mail").val();
+    var text = $("#text").val();
+    
+    if (name == "" || email == "" || text == "") {
+        alert("hi" + " " + name + " " + "we have recieved Your message .Thank you for reaching out to us")        } else {
+        alert("please make sure you have filled in the form correctly")        }    })
+
+
 $(document).click(function(){ 
     $(".design").click(function(){
         $("#design-hide").toggle();
