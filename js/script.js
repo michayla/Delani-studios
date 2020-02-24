300de747f08350f60a85ff9e91d5c6113867aa6f
Michelle- <michukimichelle@gmail.com>
2020-02-24 18:02:11 +0300

    files modified

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ $("#submit").click(function (event) {
     var name = $("#name").val();
     var mail = $("mail").val();
     var text = $("#text").val();
-    
-    if (name == "" || email == "" || text == "") {
+    //var key = '43ab10333d46e19325f96d210ed40c49-us5';//
+    if (name == "" || mail == "" || text == "") {
         alert("hi" + " " + name + " " + "we have recieved Your message .Thank you for reaching out to us")        } else {
-        alert("please make sure you have filled in the form correctly")        }    })
+        alert("please make sure you have filled in the form correctly")
+            }
+        })
 
 
 $(document).click(function(){ 
